9a0f2be715a12b6f8125a5b35a3c10052f6e79d7
Aureprz <aurelien.91440@gmail.com>
2024-04-21 21:36:37 +0200

    column fixe

--- column.h
+++ column.h
@@ -7,16 +7,62 @@ enum enum_type
 };
 typedef enum enum_type ENUM_TYPE;
 
+typedef union column_type{
+    unsigned int uint_value;
+    signed int int_value;
+    char char_value;
+    float float_value;
+    double double_value;
+    char* string_value;
+    void* struct_value;
+}COL_TYPE;
+
 typedef struct COLUMN{
-    char* Title;
-    int TPHYS;
-    int TL;
-    ENUM_TYPE TYPE;
-    void* DATA;
-    int* INDEX;
-};
+    char* title;
+    int size;
+    int max_size;
+    ENUM_TYPE column_type;
+    COL_TYPE **data;
+    unsigned long long int *index;
+}COLUMN;
 
 
+/**
+* @brief: Initialize the column
+* @param1: Type of the column
+* @param2: Title of the column as a string
+* @return: Pointer to the column
+*/
+COLUMN *create_column(ENUM_TYPE type, char *title);
+
+/**
+* @brief: Insert a new value into a column
+* @param1: Pointer to the column
+* @param2: Pointer to the value to insert
+* @return: 1 if the value is correctly inserted 0 otherwise
+*/
+int insert_value(COLUMN *col, void *value);
+
+/**
+* @brief: Free the space allocated by a column
+* @param1: Pointer to the column
+*/
+void delete_column(COLUMN **col);
+
+/**
+ * @brief: Convert a value into a string
+ * @param1: Pointer to the column
+ * @param2: Position of the value in the data array
+ * @param3: The string in which the value will be written
+ * @param4: Maximum size of the string
+ */
+void convert_value(COLUMN *col, unsigned long long int i, char *str, int size);
+
+/**
+ * @brief: Display the content of a column
+ * @param1: Pointer to the column to display
+ */
+void print_col(COLUMN* col);
 
 
 #endif //CDATAFRAME_PEREZ_LENAIN_BENSIFI_COLUMN_H
