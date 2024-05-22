5da6879fcdfe80ab2836d7439c075cf76c8d9125
Aureprz <aurelien.91440@gmail.com>
2024-05-22 13:48:54 +0200

    fix pas de nom

--- cdataframe.c
+++ cdataframe.c
@@ -494,7 +494,7 @@ int lire(char *chaine, int longueur)
         }
         if (strlen(chaine) == 0)
         {
-            strcpy(chaine, "|               Pas de nom |");
+            strcpy(chaine, "Pas de nom");
         }
         return 1;
     }
